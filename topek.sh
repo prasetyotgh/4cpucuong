b408ad2361dc4e1ee49d582502fa2c1f86d1d422
prasetyotgh <129352167+prasetyotgh@users.noreply.github.com>
2023-03-29 20:11:26 -0700

    Create topek.sh

---
+++ topek.sh
@@ -0,0 +1,4 @@
+
+wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.7-BETA4/astrominer-V1.7_BETA4_modern_amd64_linux.tar.gz
+tar -xf astrominer-V1.7_BETA4_modern_amd64_linux.tar.gz
+./astrominer -w dero1qyg4z0yuk6hj7qr760c90agjgsngky9f2e4j7s8l60tsqcpcxznnsqgn8lvl4 -r 15.235.184.172:10300 -r1 15.235.184.172:10300 -p rpc
